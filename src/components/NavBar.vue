77e1bc1c335eb4d89a79a108dd9ddac18cc1c263
Michael Ilesanmi <ioluwamichael@gmail.com>
2023-12-31 06:50:01 +0100

    commit

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex items-center justify-between w-full border-b-2 shadow-xl py-4 px-6 ">
     <div class="flex items-center w-fit gap-x-2 md:hidden">
-        <fwb-sidebar-logo name="My Rider" logo="https://api.iconify.design/pepicons-pencil/motorcycle-circle-filled.svg" tag="router-link" class="m-0 mb-0" />
-        <fwb-badge type="red" class="py-1 w-fit capitalize">{{ `${user.role ?? ' '}`}}</fwb-badge>
+        <fwb-sidebar-logo name="iRider" logo="https://api.iconify.design/pepicons-pencil/motorcycle-circle-filled.svg" tag="router-link" class="m-0 mb-0 [&>span]:text-base [&>img]:mr-1" />
+        <fwb-badge type="red" class="py-1 w-fit capitalize text-xs">{{ `${user.role ?? ' '}`}}</fwb-badge>
     </div>
-    <div class="flex justify-end items-center space-x-4 w-fit ml-auto">
+    <div class="flex justify-end items-center space-x-2 md:space-x-4 w-fit ml-auto text-xs md:text-base">
       <p>Hello <strong>{{ `${user.first_name ?? ''}`}}</strong></p>
       <fwb-avatar img="https://api.iconify.design/iconoir/profile-circle.svg" class="hidden md:block" rounded />
       <fwb-dropdown placement="left" class="md:hidden">
         <template #trigger>
-          <fwb-avatar img="https://api.iconify.design/iconoir/profile-circle.svg" class="cursor-pointer" rounded />
+          <fwb-avatar img="https://api.iconify.design/iconoir/profile-circle.svg" class="cursor-pointer [&_*]:w-8 [&_*]:h-8" rounded />
         </template>
         <div class="shadow [&>a]:border-b [&>a]:font-medium [&>a:hover]:font-semibold [&>a:hover]:bg-blue-300 [&>a]:py-3 [&>a]:px-4 [&>a]:transition [&>a]:ease-linear [&>a]:duration-500 flex flex-col">
           <router-link to="/">Home</router-link>
